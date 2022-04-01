17600551fe004635765f534926d465cdf471d30b
Jeff Pancerz <pancerzj@gmail.com>
2022-03-31 21:18:02 -0500

    fixed

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ function createDiv(){
    const block = document.createElement('div');
    block.classList.add('block');
    container.appendChild(block);
-   console.log(container);
 }
+
 
 function createGrid() {
     let row = 16;
+    let column = 15;
     for(i = 0; i < row; i++){
         createDiv()
-        for(j = 0; j < row; j++){
+        for(j = 0; j < column; j++){
             createDiv()
         }
     }
 }
+
+createGrid()
