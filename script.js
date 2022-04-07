54b6f1aca078cddb0d4b83cb1dee455f2b8e96c9
Jeff Pancerz <pancerzj@gmail.com>
2022-04-07 17:22:17 -0500

    added button functionality, with input validation

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const container = document.querySelector(".container");
+const newGrid = document.querySelector("#newGrid")
+
+newGrid.addEventListener("click", function(e){
+    customGrid()
+})
 
 function createDiv(){
    const block = document.createElement('div');
@@ -8,9 +13,7 @@ function createDiv(){
    container.appendChild(block);
 }
 
-function createGrid() {
-    let row = 16;
-    let column = 15;
+function createGrid(row=16, column=15) {
     for(i = 0; i < row; i++){
         createDiv()
         for(j = 0; j < column; j++){
@@ -19,14 +22,12 @@ function createGrid() {
     }
 }
 
-function hoverColor(){
-    block.addEventListener("mouseenter", function(e){
-        event.target.style.color = "purple";
-    })
+function customGrid(){
+    let userData = prompt('enter a grid size numerically (max 100)', '16')
+    while (userData > 100){
+     userData = prompt('please try again, your input cannot exceed 100', '16')
+    }
+    createGrid(userData);
 }
-
-
-
-
 
 createGrid()
